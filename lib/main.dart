25b653890bed03d0bfc8641c6884a12a8d685e85
Larry Geng <larry.geng12@gmail.com>
2019-04-06 16:32:54 -0400

    navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,121 @@
 import 'package:flutter/material.dart';
+import 'package:hacktj_2019/home.dart';
+import 'package:hacktj_2019/tree.dart';
+import 'package:hacktj_2019/friends.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      title: 'MoneyTrees',
+      theme: new ThemeData(
+        primaryColor: Colors.green,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MoneyTrees(title: "MoneyTrees"),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MoneyTreesState extends State<MoneyTrees> {
+  PageController _pageController;
+  int _page = 0;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+  void initState() {
+    super.initState();
+    _pageController = new PageController();
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
 
-  void _incrementCounter() {
+  void navigationTapped(int page) {
+    // Animating to the page.
+    // You can use whatever duration and curve you like
+    _pageController.animateToPage(page,
+        duration: const Duration(milliseconds: 200), curve: Curves.ease);
+  }
+
+  void onPageChanged(int page) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      this._page = page;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(
+          widget.title,
+          style: new TextStyle(color: Colors.white),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: new PageView(
+        children: [
+          new Home("Home screen"),
+          new Tree("Tree screen"),
+          new Friends("Friends screen"),
+        ],
+        onPageChanged: onPageChanged,
+        controller: _pageController,
+      ),
+      bottomNavigationBar: new Theme(
+        data: Theme.of(context).copyWith(
+          // sets the background color of the `BottomNavigationBar`
+          canvasColor: Colors.green,
+        ), // sets the inactive color of the `BottomNavigationBar`
+        child: new BottomNavigationBar(
+          items: [
+            new BottomNavigationBarItem(
+                icon: new Icon(
+                  Icons.home,
+                  color: Colors.white,
+                ),
+                title: new Text(
+                  "Home",
+                  style: new TextStyle(
+                    color: Colors.white,
+                  ),
+                )),
+            new BottomNavigationBarItem(
+                icon: new Icon(
+                  Icons.nature,
+                  color: Colors.white,
+                ),
+                title: new Text(
+                  "Tree",
+                  style: new TextStyle(
+                    color: Colors.white,
+                  ),
+                )),
+            new BottomNavigationBarItem(
+                icon: new Icon(
+                  Icons.people,
+                  color: Colors.white,
+                ),
+                title: new Text(
+                  "Friends",
+                  style: new TextStyle(
+                    color: Colors.white,
+                  ),
+                ))
+          ],
+          onTap: navigationTapped,
+          currentIndex: _page,
+        ),
+      ),
     );
   }
+}
+
+class MoneyTrees extends StatefulWidget {
+  MoneyTrees({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  MoneyTreesState createState() => new MoneyTreesState();
 }
