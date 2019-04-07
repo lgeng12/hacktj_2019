06ed991b96744959d7764fc046cb67ab523d3b45
Larry Geng <larry.geng12@gmail.com>
2019-04-07 07:51:37 -0400

    final changes

--- lib/main.dart
+++ lib/main.dart
@@ -190,7 +190,7 @@ class _MoneyTreesState extends State<MoneyTrees> {
       newInfo.treeCondition -= 1;
 
     // update numTrees
-    newInfo.numTrees = newInfo.balance ~/ 100 + 1;
+    newInfo.numTrees += newInfo.balance ~/ 10;
     newInfo.amountSpent = 0.0;
     newInfo.balance = 30;
     writeJson(json.encode(newInfo));
